a17c33329129a9e38d6c4d3a79bac4072515d2a2
sbx_user1051 <sbx_user1051@169.254.1.9>
2019-10-11 23:57:32 +0000

    Fri Oct 11 23:57:32 UTC 2019

--- src/main/java/befaster/solutions/SUM/SumSolution.java
+++ src/main/java/befaster/solutions/SUM/SumSolution.java
@@ -6,7 +6,22 @@ import befaster.runner.SolutionNotImplementedException;
 public class SumSolution {
 
     public int compute(int x, int y) {
-        throw new SolutionNotImplementedException();
+
+        // x and y must be between 0 and 100, I assume including 100
+
+        if((x<=100)&&(x>=0)){
+            if((y<=100)&&(y>=0)){
+                return x+y;
+            }
+        }
+        else{
+            // not in the instruction, return -1 if invalid parameters are given.
+            return -1;
+        }
+
+
+
     }
 
 }
+
