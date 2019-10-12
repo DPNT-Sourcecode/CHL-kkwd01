cce615acd7c2deb8f9ca7f873045aa338abef214
sbx_user1051 <sbx_user1051@169.254.1.37>
2019-10-12 02:02:01 +0000

    Sat Oct 12 02:02:01 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -87,24 +87,11 @@ public class CheckoutSolution {
         // 2 F get one free
         // we always need to have 3 in the basket
         if(countF>2){
-               /*
-              int extraF=countF-2; // we need to keep at least 3 in the basket
-              price=price+(2*10);
+               // now i undestand the question, each time we have 3 fs it will become 2.
+               int pair_3f=countF/3;
 
-              int number_free_F=extraF/2;
+               countF=countF-pair_3f;
 
-              extraF=extraF-number_free_F;
-              if(extraF<0){
-                  extraF=0;
-
-              }
-              price=price+(extraF*10);
-              */
-               int pair_F=countF/2;
-               countF=countF-pair_F;
-               if(countF<0){
-                   countF=0;
-               }
                price = price + (countF * 10);
 
 
@@ -125,3 +112,4 @@ public class CheckoutSolution {
 
     }
 }
+
