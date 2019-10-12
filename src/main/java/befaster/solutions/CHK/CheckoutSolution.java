21467f42e01ff3a5aba8091b1d35903490213fed
sbx_user1051 <sbx_user1051@169.254.1.9>
2019-10-12 00:32:10 +0000

    Sat Oct 12 00:32:10 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -8,11 +8,11 @@ public class CheckoutSolution {
 
         //A and B have special offers, we are going to count how many of them we are going to have.
         int countA=0;
-        int CountB=0;
+        int countB=0;
 
         // we add to this variable as we detect each sku
         int price=0;
-
+        boolean illegalinput=false; //illegal input any letter other than skus,
 
         for (int i=0;i<stringsize;i++){
              // going to through each character of string one by one
@@ -24,7 +24,7 @@ public class CheckoutSolution {
 
                    break;
                 case 'B':
-                    CountB++;
+                    countB++;
                     break;
                 case 'C':
                       price=price+20;
@@ -33,17 +33,33 @@ public class CheckoutSolution {
                       price=price+15;
                     break;
                 default:
+                    illegalinput=true;
 
             }
-            
 
+
+
+        }
+
+        //3A =150
+        int discountsA= countA/3; // with discount
+        int singleA=countA%3; // without discount
+        price=price+(discountsA*130)+(singleA*50);
+
+        int discountsB= countB/2; // with discount
+        int singleB=countB%2; // without discount
+        price=price+(discountsB*45)+(singleB*30);
+
+        if(illegalinput==false) {
+            return price;
+        }else{
+            return -1;
 
         }
 
 
 
-
-
     }
 }
+
 
